a45c02598e196ea0a7d49d651ad84b8763cac0a8
Chiara <chiara.magosso@studenti.polito.it>
2025-05-16 09:40:52 +0200

    cc by license

--- sem_image_automated_preprocessing_for_ADAblock.py
+++ sem_image_automated_preprocessing_for_ADAblock.py
@@ -4,8 +4,8 @@
 ## 
 ## This work is licensed under a  
 ## 
-## Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
-## ( http://creativecommons.org/licenses/by-nc-sa/4.0/ )
+## Creative Commons Attribution 4.0 International License
+## ( https://creativecommons.org/licenses/by/4.0/ )
 ## 
 ## Please contact chiara.magosso@polito.it for information.
 ##
